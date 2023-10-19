48467d3d0ceb28ec44473da82621eba408c532f5
samssjubair <samssjubair@gmail.com>
2023-10-19 20:45:53 +0600

    booking cancel from user side

--- src/components/booking/Booking.tsx
+++ src/components/booking/Booking.tsx
@@ -1,12 +1,36 @@
 import dayjs from "dayjs";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Stepper from "../ui/Steps";
+import axios from "axios";
+import { getFromLocalStorage } from "@/utils/local-storage";
+import { authKey } from "@/constants/storage";
+import { getBaseUrl } from "@/helpers/config/envConfig";
+import { message } from "antd";
 
 const BookingCard = ({ booking }: { booking: any }) => {
-  const { service } = booking; // Destructure the service object from the booking
+  const { service, status } = booking; // Destructure the service object and status from the booking
+  // const [isCancelled, setIsCancelled] = useState(false);
 
-  
+  // Function to cancel the order
+  const cancelOrder = async () => {
+    axios.delete(`${getBaseUrl()}/bookings/${booking.id}`, {
+      headers: {
+        Authorization: `${getFromLocalStorage(authKey)}`,
+      },
+    })
+    .then((res) => {
+      res.data;
+      // message.success("Booking Cancelled Successfully").then(() => window.location.reload());
+      message.success("Booking Cancelled Successfully").then(() => window.location.reload());
+      // setIsCancelled(true);
+    })
+    .catch((err) => {
+      message.error("Booking Cancelled Failed");
+      console.error(err.message);
+    });
+
+  };
 
   return (
     <div className="bg-white rounded-lg shadow-md p-4 m-4">
@@ -17,12 +41,20 @@ const BookingCard = ({ booking }: { booking: any }) => {
       <p className="text-gray-600">Price: ${service.price}</p>
       <p className="text-gray-600">Organization: {service.organization}</p>
       <p className="text-gray-600">Subcategory: {service.subCategory}</p>
-      <p className="text-gray-600">Organization: {service.organization}</p>
-      <p className="text-gray-600">Description: {service.escription}</p>
+      <p className="text-gray-600">Description: {service.description}</p>
       <div className="my-4">
-        <Stepper status={booking.status}/>
+        <Stepper status={status} />
       </div>
 
+      {/* Render the "Cancel Order" button if the order is not cancelled */}
+     
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+          onClick={cancelOrder}
+        >
+          Cancel Order
+        </button>
+     
       {service.imageUrl && (
         <Image
           src={service.imageUrl}
