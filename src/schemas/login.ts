a74e1bf9bcc3fc508d53f4ccb4f3290fc13c37bc
samssjubair <samssjubair@gmail.com>
2023-10-20 19:38:22 +0600

    login and signup validation issue solve

--- src/schemas/login.ts
+++ src/schemas/login.ts
@@ -2,5 +2,24 @@ import * as yup from "yup";
 
 export const loginSchema = yup.object().shape({
   email: yup.string().email().required("Email is required"),
-  password: yup.string().min(6).max(32).required(),
+  password: yup
+    .string()
+    .min(6)
+    .max(32)
+    .required("Password is required")
+    .test(
+      "password",
+      "Password must contain at least one uppercase letter, one special character, and one number",
+      (value) => {
+        const uppercaseRegex = /[A-Z]/;
+        const specialCharRegex = /[!@#$%^&*]/; // Add more special characters as needed
+        const numberRegex = /[0-9]/;
+
+        return (
+          uppercaseRegex.test(value) &&
+          specialCharRegex.test(value) &&
+          numberRegex.test(value)
+        );
+      }
+    ),
 });
--- src/schemas/signup.ts
+++ src/schemas/signup.ts
@@ -3,7 +3,26 @@ import * as Yup from "yup";
 export const signupSchema = Yup.object().shape({
   name: Yup.string(),
   email: Yup.string().email().required("Email is required"),
-  password: Yup.string().min(6).max(32).required(),
+  password: Yup
+    .string()
+    .min(6)
+    .max(32)
+    .required("Password is required")
+    .test(
+      "password",
+      "Password must contain at least one uppercase letter, one special character, and one number",
+      (value) => {
+        const uppercaseRegex = /[A-Z]/;
+        const specialCharRegex = /[!@#$%^&*]/; // Add more special characters as needed
+        const numberRegex = /[0-9]/;
+
+        return (
+          uppercaseRegex.test(value) &&
+          specialCharRegex.test(value) &&
+          numberRegex.test(value)
+        );
+      }
+    ),
   contactNo: Yup.string(),
   address: Yup.string(),
 });
